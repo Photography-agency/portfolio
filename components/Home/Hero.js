1b6d59ef5dfbf6fda59caabfd9d74a2f49a59b6b
redwan10110 <redwan.hoque.dev@gmail.com>
2023-03-20 21:43:42 +0530

    home structure is done

---
+++ components/Home/Hero.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./Hero.module.css";
+// import logoImg from "/public/img/logo.png";
+// import smallMountain from "/public/img/landscape_mountain_small.png";
+
+const Hero = () => {
+  return (
+    <>
+      
+      
+    </>
+  );
+};
+
+export default Hero;
---
+++ components/Home/ServicesNav.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ServicesNav = () => {
+  return (
+    <div>ServicesNav</div>
+  )
+}
+
+export default ServicesNav;
